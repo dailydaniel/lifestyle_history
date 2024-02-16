377feb3242f64fbd58e73ee1fd811fcefa80b2d1
mandanya <dan.zholk@gmail.com>
2024-02-16 19:24:32 +0300

    add poopee

--- app.py
+++ app.py
@@ -18,7 +18,8 @@ st.set_page_config(
     layout="wide",
 )
 
-key = '1CZqJ-z0i2yGqFr5dTOG2YOYngKx8ngVVqsnPbja95-A'
+key_life = '1CZqJ-z0i2yGqFr5dTOG2YOYngKx8ngVVqsnPbja95-A'
+key_poopee = '1GfLQYA0g04Rjib0xMtOzwGA_vnJP0NSGuZLENI71EgI'
 weight_goal = 69.0
 
 
@@ -68,7 +69,7 @@ def h2n(s: str) -> str:
 
 
 # @st.cache_data
-def get_data(filter_period: int = None) -> pd.DataFrame:
+def get_data(filter_period: int = None, key: str = key_life) -> pd.DataFrame:
     df = pd.read_csv('https://docs.google.com/spreadsheets/d/' +
                      key +
                      '/export?gid=0&format=csv',
@@ -95,6 +96,22 @@ def get_data(filter_period: int = None) -> pd.DataFrame:
     return df
 
 
+def get_poopee_data(filter_period: int = None, key: str = key_poopee) -> pd.DataFrame:
+    df = pd.read_csv('https://docs.google.com/spreadsheets/d/' +
+                     key +
+                     '/export?gid=0&format=csv',
+                     parse_dates=['Date'],
+                     dayfirst=True)
+
+    if filter_period:
+        date = pd.Timestamp.now()
+        delta_date = pd.to_timedelta(f'{filter_period * 7} days')
+        period_date = date - delta_date
+        df = df[df['Date'].dt.date >= period_date.date()]
+
+    return df
+
+
 colors = dict(matplotlib.colors.cnames.items())
 hex_colors = tuple(colors.values())
 
@@ -103,7 +120,7 @@ st.markdown("Start: 2024-02-16. Logbook of my Health.")
 st.markdown("Powered by google sheet and siri shortcuts.")
 url_tg = "https://t.me/mandanya77"
 st.markdown("made by Daniel Zholkovsky [telegram](%s)" % url_tg)
-st.markdown("Version 0.14")
+st.markdown("Version 0.15")
 
 filter_period = st.selectbox("Select num weeks:", ["All", 4, 1])
 filter_period = None if filter_period == "All" else filter_period
@@ -112,10 +129,12 @@ placeholder = st.empty()
 
 while True:
     df = get_data(filter_period=filter_period)
+    df_poopee = get_poopee_data(filter_period=filter_period)
 
-    real_types = df['Type'].dropna().unique().tolist()
-    cur_colors = choices(hex_colors, k=len(real_types))
-    type2color = {rt: col for rt, col in zip(real_types, cur_colors)}
+    # real_types = df_poopee['Type'].dropna().unique().tolist()
+    # cur_colors = choices(hex_colors, k=len(real_types))
+    # type2color = {rt: col for rt, col in zip(real_types, cur_colors)}
+    type2color = {'Poope': 'brown', 'Pee': 'green'}
 
     with placeholder.container():
         df_gb = get_gb(df)
@@ -147,12 +166,9 @@ while True:
         col1, col2 = st.columns(2)
 
         with col1:
-            st.markdown(f"<h4 style='text-align: center;'>Хуй говно жопа</h4>", unsafe_allow_html=True)
-
-        with col2:
             txt = f"Health chart by {filter_period}" if filter_period else "Health chart"
             st.markdown(f"<h4 style='text-align: center;'>{txt}</h1>", unsafe_allow_html=True)
-            fig2 = go.Figure(
+            fig1 = go.Figure(
                 layout=dict(
                     xaxis=dict(categoryorder="category descending"),
                     yaxis=dict(range=[-0.05, 1.05]),
@@ -161,7 +177,7 @@ while True:
                 )
             )
 
-            fig2.add_trace(
+            fig1.add_trace(
                 go.Bar(
                     x=df_gb[df_gb['Type'] == 'Eat']['Date'],
                     y=df_gb[df_gb['Type'] == 'Eat']['Kk_num'],
@@ -171,7 +187,7 @@ while True:
                 )
             )
 
-            fig2.add_trace(
+            fig1.add_trace(
                 go.Bar(
                     x=df_gb[df_gb['Type'] == 'Sleep']['Date'],
                     y=df_gb[df_gb['Type'] == 'Sleep']['Hours_num'],
@@ -181,7 +197,7 @@ while True:
                 )
             )
 
-            fig2.add_trace(
+            fig1.add_trace(
                 go.Bar(
                     x=df_gb[df_gb['Type'] == 'Train']['Date'],
                     y=df_gb[df_gb['Type'] == 'Train']['Hours_num'],
@@ -191,7 +207,7 @@ while True:
                 )
             )
 
-            fig2.add_trace(
+            fig1.add_trace(
                 go.Scatter(
                     x=df_gb[df_gb['Type'] == 'Weight']['Date'],
                     y=df_gb[df_gb['Type'] == 'Weight']['Kg_num'],
@@ -202,19 +218,35 @@ while True:
                 )
             )
 
-            fig2.add_hline(
+            fig1.add_hline(
                 y=1, line_width=3,
                 line_dash="dash",
                 line_color="red",
                 annotation_text=f"Goal: {weight_goal}kg",
             )
 
-            fig2.update_xaxes(tickformat="%d %b")
-            fig2.update_layout(
+            fig1.update_xaxes(tickformat="%d %b")
+            fig1.update_layout(
                 autosize=False,
                 width=550,
                 height=400
             )
+
+            st.write(fig2)
+
+        with col2:
+            txt = f"Poopee chart by {filter_period}" if filter_period else "Poopee chart"
+            st.markdown(f"<h4 style='text-align: center;'>{txt}</h1>", unsafe_allow_html=True)
+
+            df_gb_poopee = df_poopee.groupby(pd.Grouper(key='Date', freq='D'))['Type'].value_counts().reset_index()
+            fig2 = px.bar(data_frame=df_gb_poopee, y='count', x='Date', color='Type')
+            fig2.update_layout(legend=dict(yanchor="top", y=1.2, xanchor="left", x=0.01))
+            fig2.update_layout(margin=dict(l=50, r=50))
+            fig2.update_yaxes(nticks=5)
+
+            for i in range(len(fig2.data)):
+                name = fig2.data[i].name
+                fig2.data[i].marker.color = type2color[name]
 
             st.write(fig2)
 
