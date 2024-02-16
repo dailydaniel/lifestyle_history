63d7966faf2d3e6ec70a63cd95e56a483868d90f
mandanya <dan.zholk@gmail.com>
2024-02-16 18:14:22 +0300

    fix vis

--- app.py
+++ app.py
@@ -107,7 +107,7 @@ st.markdown("Start: 2024-02-16. Logbook of my Health.")
 st.markdown("Powered by google sheet and siri shortcuts.")
 url_tg = "https://t.me/mandanya77"
 st.markdown("made by Daniel Zholkovsky [telegram](%s)" % url_tg)
-st.markdown("Version 0.1")
+st.markdown("Version 0.2")
 
 filter_period = st.selectbox("Select num weeks:", ["All", 4, 1])
 filter_period = None if filter_period == "All" else filter_period
@@ -135,7 +135,8 @@ while True:
             st.markdown(f"<h4 style='text-align: center;'>Хуй говно жопа</h4>", unsafe_allow_html=True)
 
         with col2:
-            st.markdown(f"<h4 style='text-align: center;'>Bar chart by {filter_period}</h1>", unsafe_allow_html=True)
+            txt = f"Bar chart by {filter_period}" if filter_period else "Bar chart"
+            st.markdown(f"<h4 style='text-align: center;'>{txt}</h1>", unsafe_allow_html=True)
             df_gb = get_gb(df)
             fig2 = go.Figure(
                 layout=dict(
@@ -187,12 +188,16 @@ while True:
                 )
             )
 
-            fig2.update_xaxes(
-                tickformat="%d %b")
+            fig2.update_xaxes(tickformat="%d %b")
+            fig2.update_layout(
+                autosize=False,
+                width=550,
+                height=400
+            )
 
             st.write(fig2)
 
         st.markdown("### Full Table")
         st.dataframe(df)
 
-    time.sleep(3600)
+    time.sleep(15)
